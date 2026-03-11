396edde6b48837b2ade2c69a99a3963823da5af5
Anisha Vulli <anishavulli2@gmail.com>
2026-03-11 15:10:50 -0400

    code1Commit

---
+++ CodingPractice/Problem1/CountElements.java
@@ -0,0 +1,26 @@
+package CodingPractice.Problem1;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountElements {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] inputArr = input.split("");
+        HashMap<String,Integer> countMap = new HashMap<>();
+        for(String s : inputArr) {
+            // if(countMap.containsKey(s)) {
+            //     int value = countMap.get(s);
+            //     countMap.put(s, value+1);
+            // } else {
+            //     countMap.put(s,1);
+            // }
+            countMap.computeIfPresent(s, (key,val) -> val + 1);
+            countMap.computeIfAbsent(s,  key -> 1);
+        }
+        System.err.println(countMap);
+        sc.close();
+    }
+}
