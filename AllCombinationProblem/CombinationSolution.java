6e9fc3b893c5a2436cfcca6cebf639c82d61326a
Anisha Vulli <anishavulli2@gmail.com>
2026-03-18 15:08:27 -0400

    day13:Base commit

---
+++ AllCombinationProblem/CombinationSolution.java
@@ -0,0 +1,15 @@
+package AllCombinationProblem;
+
+import java.util.*;
+
+public class CombinationSolution {
+    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number for which we need combinations: ");
+            int number = sc.nextInt();
+            List<Integer> currrent = new ArrayList<>();
+        }
+    }
+
+}
